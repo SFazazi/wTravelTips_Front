4b8e42df2f0193ec5da62f4b5b431a7a43832189
SFazazi <mohammedfazazi@gmail.com>
2018-03-24 00:07:54 +0100

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
+import {RouterModule, Routes} from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdviceComponent } from './advice/advice.component';
+import { DestinationComponent } from './destination/destination.component';
+import { OfferComponent } from './offer/offer.component';
+import { AboutComponent } from './about/about.component';
+import { InfoComponent } from './info/info.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'advice', component: AdviceComponent },
+  { path: 'destination', component: DestinationComponent },
+  { path: 'offer', component: OfferComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'notfound', component: NotfoundComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdviceComponent,
+    DestinationComponent,
+    OfferComponent,
+    AboutComponent,
+    InfoComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
